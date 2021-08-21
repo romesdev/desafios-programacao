b7b068a66fd33ad9f71ece0197466abb8938fa67
romesdev <fs.romes@gmail.com>
2021-08-20 23:17:27 -0300

    up extra na semana 7

--- desafios da disciplina/semana 7/extra/max_consecutive_ones.cpp
+++ desafios da disciplina/semana 7/extra/max_consecutive_ones.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+        int maxOne = 0;
+        int one = 0;
+        
+        int j = 0;
+        
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] == 0) one = 0;
+            
+            if (nums[i] == 1){
+               
+                for (j = i; j < nums.size(); j++){
+                    if (nums[j] == 0) break;
+                    one++;
+                }     
+                
+                i = j;                 
+            }
+            
+            if (one > maxOne) maxOne = one;
+            one = 0; 
+            
+            
+        }            
+        
+        
+        return maxOne;
+        
+        
+        
+        /* outra alternativa
         int maxOne = 0;
         int one = 0;
         
@@ -17,9 +48,6 @@ public:
         
         
         return maxOne;
-        
-        
-        
-        
+        */
     }
 };
